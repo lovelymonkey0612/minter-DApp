873e62119cf22835994b4da29917d91c14735eb7
Denis Martin <denysmartin4952@gmail.com>
2024-02-26 03:45:44 -0800

    initial project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Minter from "./Minter";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Minter />
+    </div>
+  );
+}
+
+export default App;
